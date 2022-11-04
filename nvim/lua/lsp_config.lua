7cf31d73a999d912d77db7d19f4f2a3a595b2902
Oscar De Leon Gomez <odeleongomez@rivian.com>
2022-11-04 16:07:36 -0700

    Added beautifying stuff

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -69,7 +69,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local flags = { debounce_text_changes = 150 }
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 -- Mappings.
@@ -164,7 +164,6 @@ require 'lspconfig'.sumneko_lua.setup {
             },
         },
     },
-  },
 }
 
 require 'lspconfig'.tsserver.setup({
